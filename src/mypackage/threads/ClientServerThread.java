403e64765bf3cbab8ac66af0c0f0c8eb91596690
user <toropov.nikita2004@gmail.com>
2022-12-01 14:48:43 +0100

    Substitute an if statement with a switch

--- src/mypackage/threads/ClientServerThread.java
+++ src/mypackage/threads/ClientServerThread.java
@@ -23,30 +23,29 @@ public class ClientServerThread extends Thread{
             PrintWriter pw = new PrintWriter(serverSocket.getOutputStream(), true);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
             String firstLine = bufferedReader.readLine();
-            if(firstLine.equals("Connect node")){
-                String newNode = bufferedReader.readLine();
-                node.getConnectedNodes().add(newNode);
-                System.out.println(node.getIp() + " " +node.getPort() + " is connected to: ");
-                for(String s : node.getConnectedNodes()){
-                    System.out.print(s + " ");
+            switch (firstLine) {
+                case "Connect node" -> {
+                    String newNode = bufferedReader.readLine();
+                    node.getConnectedNodes().add(newNode);
+                    System.out.println(node.getIp() + " " + node.getPort() + " is connected to: ");
+                    for (String s : node.getConnectedNodes()) {
+                        System.out.print(s + " ");
+                    }
+                    System.out.println();
                 }
-                System.out.println();
-
-            }
-            else if(firstLine.equals("Serve client")){
-                pw.println("Connected: " + serverSocket.getLocalPort());
-                node.iterateOverNetwork(new ArrayList<>());
-            }
-            else if(firstLine.equals("Serve node")){
-                pw.println(node.getPort() + " - " + node.getKey() + ":"+node.getValue());
-
-
-            }
-            else if(firstLine.equals("Provide node")){
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(serverSocket.getOutputStream());
-                objectOutputStream.writeObject(node);
-                objectOutputStream.flush();
-                objectOutputStream.close();
+                case "Serve client" -> {
+                    pw.println("Connected: " + serverSocket.getLocalPort());
+                    node.iterateOverNetwork(new ArrayList<>());
+                }
+                case "Serve node" -> {
+                    pw.println(node.getPort() + " - " + node.getKey() + ":" + node.getValue());
+                }
+                case "Provide node" -> {
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(serverSocket.getOutputStream());
+                    objectOutputStream.writeObject(node);
+                    objectOutputStream.flush();
+                    objectOutputStream.close();
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
